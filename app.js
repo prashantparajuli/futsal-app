e6541560d0e77cb396af61644fadefe8378a9585
prashant parajuli <prashanta.parajuli@macpokhara.edu.np>
2022-08-13 12:28:39 +0545

    ntmuch

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
+// const passport = require('passport');
+
 const routes = require('./routes');
 require('dotenv').config;
 require('./config').dbConnect;
 
+// const initializePassport = require('./middlewares/passportAuth');
+// initializePassport(passport);
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// app.use(session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false
+// }))
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 app.use('/api', routes)
 const port = process.env.PORT;
